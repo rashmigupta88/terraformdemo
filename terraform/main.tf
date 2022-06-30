352c8b46546ebf3ba05ef1368a174ab13f2daca0
bloggernest <44660748+rashmigupta88@users.noreply.github.com>
2022-06-29 21:41:59 -0400

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = "=2.4.0"
+  features {}
+}
+
+# 3. Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "East US"
+}
+
+outpot :id" {
+ value = data.azurerm_resource_group.example.id
+ 
+ }

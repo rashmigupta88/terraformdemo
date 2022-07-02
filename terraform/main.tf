3cd15c146c9624adda03bb8f6c1b7ad5283f059e
bloggernest <44660748+rashmigupta88@users.noreply.github.com>
2022-07-02 07:35:58 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,21 @@ terraform{
 backened "azure" {}
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  version = "=2.4.0"
   features {}
 }
+
+
 
 # 3. Create a resource group
 resource "azurerm_resource_group" "example" {
